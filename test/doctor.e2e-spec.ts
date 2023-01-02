337be0aa87bc476a353b84fb7eb89f780590b25f
LeonMesquita <leonmesquita16@gmail.com>
2023-01-02 13:42:56 -0300

    test: return 200 the doctor crm was updated

--- test/doctor.e2e-spec.ts
+++ test/doctor.e2e-spec.ts
@@ -89,4 +89,14 @@ describe('DoctorController (e2e)', () => {
       })
       .expect(200);
   });
+
+  it('should update the doctor crm', async () => {
+    return request(app.getHttpServer())
+      .put('/doctor/1')
+      .send({
+        ...doctor,
+        crm: '4536791',
+      })
+      .expect(200);
+  });
 });
