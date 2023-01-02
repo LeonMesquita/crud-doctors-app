f0da1c75bbc990ef749bde1d7e7112f40a60be1a
LeonMesquita <leonmesquita16@gmail.com>
2023-01-02 13:07:45 -0300

    test: return 200 if a doctor was returned by his crm

--- test/doctor.e2e-spec.ts
+++ test/doctor.e2e-spec.ts
@@ -42,4 +42,7 @@ describe('DoctorController (e2e)', () => {
   it('should get a doctor by id', async () => {
     return request(app.getHttpServer()).get('/doctor/1').expect(200);
   });
+  it('should get a doctor by crm', async () => {
+    return request(app.getHttpServer()).get('/doctor/4536784').expect(200);
+  });
 });
